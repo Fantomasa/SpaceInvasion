51fc9c53369a4a2ff9a04cd76a1827041b43c65d
Fantomasa <ivailoinfo@gmail.com>
2020-08-11 10:50:25 +0300

    Add Bonus Bar file

---
+++ Assets/Scripts/Bars/BonusBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Check bonus bar.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
